654c7982f14c17c3f0f4a283f9d709a7bc2cac79
Дима Крымских <krymskikh@gmail.com>
2023-02-04 17:50:57 +0700

    Добавил логирование

--- app/App.php
+++ app/App.php
@@ -20,6 +20,10 @@ class App
     public static object $data;
     public static ?object $request;
     public static int $userId;
+    
+    private string $requestMethod;
+    private string $truncatedUri;
+    private string|false $response;
 
     public function __construct(object $config) 
     {
@@ -39,9 +43,15 @@ class App
 //        sleep(1);
         
         try {
+            ob_start();
             $this->router();
+            $this->response = ob_get_clean();
+            $this->writeLogs('access.log');
         } catch (ResponseException $e) {
-            echo $e->getResponse();
+            $this->response = $e->getResponse();
+            $this->writeLogs('errors.log');
+        } finally {
+            echo $this->response;
         }
     }
 
@@ -58,16 +68,16 @@ class App
         header('Access-Control-Allow-Credentials: true');
         header('Content-Type: application/json');
         
-        $requestMethod = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
+        $this->requestMethod = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
         // Если метод запроса "OPTIONS", то ничего не делаем
         // (Браузеры отправляют предзапрос с методом "OPTIONS", если запрос не является простым, т.е. нарушено одно из правил простого запроса.
         // См. https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
         // Например, тип заголовка "Content-Type: application/json" не допустим для простого запроса)
-        if (mb_strtolower($requestMethod) === 'options') {
+        if (mb_strtolower($this->requestMethod) === 'options') {
             exit();
         }
         
-        $truncatedUri = trim(parse_url(filter_input(INPUT_SERVER, 'REQUEST_URI'), PHP_URL_PATH), '/');
+        $this->truncatedUri = trim(parse_url(filter_input(INPUT_SERVER, 'REQUEST_URI'), PHP_URL_PATH), '/');
         
         self::$request = json_decode(file_get_contents("php://input"));
         
@@ -92,7 +102,21 @@ class App
         // Формируем массив маршрутов
         require_once __DIR__ . '/../routes/web.php';
         // Находим и выполняем экшен
-        $router->run($requestMethod, $truncatedUri);
+        $router->run($this->requestMethod, $this->truncatedUri);
         
+    }
+    
+    private function writeLogs(string $file): void
+    {
+        $log = date('Y-m-d H:i:s') . PHP_EOL;
+        foreach (getallheaders() as $name => $value) {
+            $log .= "$name: $value\n";
+        }
+        $log .= "Метод запроса: $this->requestMethod\n";
+        $log .= "Uri запроса: $this->truncatedUri\n";
+        $log .= "Получены данные:" . PHP_EOL . print_r(self::$request, true);
+        $log .= "Отправлены данные:" . PHP_EOL . print_r(json_decode($this->response), true);
+
+        file_put_contents(dirname(__DIR__) . "/storage/logs/$file", $log . PHP_EOL, FILE_APPEND);
     }
 }
